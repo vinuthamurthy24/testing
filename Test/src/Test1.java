55acb0289989d92bf8d0c439f0b865134a82a0de
vinutha <vinutha@DESKTOP-5G761I8>
2020-08-11 13:38:57 +0530

    first commit by vinutha

---
+++ Test/src/Test1.java
@@ -0,0 +1,24 @@
+import java.time.format.DateTimeFormatter;
+
+import java.time.LocalDateTime;  
+
+  
+   
+ 
+
+public class Test1 {
+
+	public static void main(String args[]){
+		
+		java.util.Date date=new java.util.Date();  
+
+		System.out.println(date);  
+		System.out.println("date:"+date);  
+		/*DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		   LocalDateTime now = LocalDateTime.now();
+		   System.out.println(dtf.format(now));*/
+		/*Date date = new Date();
+		long time=date.getTime();
+		System.out.println("time::"+time);*/
+	}
+}
